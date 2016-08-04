053f5fba2dda705e385331bbee1e1292ef5db64d
albert.m <albert.m@quali.com>
2016-08-04 21:44:49 +0300

    Created shells-dashboard page (Default.html) which displays all the available shells and a search box.

---
+++ QpmServer/Scripts/ShellsDashboardController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('ShellRepoApp')
+        .controller('ShellsDashboardController', ['$rootScope', '$scope', 'ShellsService', ShellsDashboardController]);
+
+    function ShellsDashboardController($rootScope, $scope, shellsService) {
+
+        var self = this;
+
+        self.shells = undefined;
+
+        shellsService.getAllShells().then(function (response) {
+            self.shells = response.data;
+        });
+    }
+
+})();
