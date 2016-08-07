25df08d94ca63d455033f385b6cf137c565f280a
Boris Modylevsky <borismod@gmail.com>
2016-08-07 08:43:58 +0300

    add elastic search using NEST

---
+++ QpmServer/Engine/SearchingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nest;
+using ShellRepo.Models;
+
+namespace ShellRepo.Engine
+{
+    public interface ISearchingService
+    {
+        List<ShellEntity> Search(string text);
+    }
+
+    public class SearchingService : ISearchingService
+    {
+        private readonly ElasticClient client;
+
+        public SearchingService(IShellRepoConfiguration shellRepoConfiguration)
+        {
+            var settings = new ConnectionSettings(new Uri(shellRepoConfiguration.ElasticSearchUrl));
+            settings.DefaultIndex("shell");
+
+            client = new ElasticClient(settings);
+        }
+
+        public List<ShellEntity> Search(string text)
+        {
+            return
+                client.Search<ShellEntity>(_ => _.From(0).Size(10).Query(q => q.Term(s => s.Name, text)))
+                    .Documents.ToList();
+        }
+    }
+}
--- QpmServer/Engine/ShellRepoConfiguration.cs
+++ QpmServer/Engine/ShellRepoConfiguration.cs
@@ -1,10 +1,12 @@
 ﻿using System.Web.Configuration;
 
-namespace ShellRepo.Controllers
+namespace ShellRepo.Engine
 {
     public interface IShellRepoConfiguration
     {
         string MongoConnectionString { get; }
+        string ElasticSearchUrl { get; }
+        string ElasticSearchSecureUrl { get; }
     }
 
     public class ShellRepoConfiguration : IShellRepoConfiguration
@@ -12,6 +14,14 @@ namespace ShellRepo.Controllers
         public string MongoConnectionString
         {
             get { return WebConfigurationManager.AppSettings["MONGOLAB_URI"]; }
+        }
+        public string ElasticSearchUrl
+        {
+            get { return WebConfigurationManager.AppSettings["SEARCHBOX_URL"]; }
+        }
+        public string ElasticSearchSecureUrl
+        {
+            get { return WebConfigurationManager.AppSettings["SEARCHBOX_SSL_URL"]; }
         }
     }
 }
